45df944a6b56d49db6aafc6c16943b90368dee84
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-06-17 15:42:56 -0300

    trying to use git commands from within js

--- ci_linter_helper.js
+++ ci_linter_helper.js
@@ -1,9 +1,52 @@
-const {
-    GITHUB_EVENT_PATH, GITHUB_SHA, GITHUB_TOKEN, GITHUB_WORKSPACE, ESLINT_RC, EXECUTE_ON_FILES
-  } = process.env;
+// const {
+//     GITHUB_EVENT_PATH, GITHUB_SHA, GITHUB_TOKEN, GITHUB_WORKSPACE, ESLINT_RC, EXECUTE_ON_FILES
+//   } = process.env;
 
-console.log(GITHUB_EVENT_PATH, GITHUB_SHA, GITHUB_TOKEN, GITHUB_WORKSPACE, ESLINT_RC, EXECUTE_ON_FILES)
+// console.log(GITHUB_EVENT_PATH, GITHUB_SHA, GITHUB_TOKEN, GITHUB_WORKSPACE, ESLINT_RC, EXECUTE_ON_FILES)
 
-const event = require(GITHUB_EVENT_PATH);
+// const event = require(GITHUB_EVENT_PATH);
 
-console.log(event)
+// console.log(event)
+
+// const { ESLint } = require("eslint");
+
+// (async function main() {
+//   // 1. Create an instance.
+//   const eslint = new ESLint();
+
+//   // 2. Lint files.
+//   const results = await eslint.lintFiles(["lib/**/*.js"]);
+
+//   // 3. Format the results.
+//   const formatter = await eslint.loadFormatter("json");
+//   const resultText = formatter.format(results);
+
+//   // 4. Output it.
+//   console.log(resultText);
+// })().catch((error) => {
+//   process.exitCode = 1;
+//   console.error(error);
+// });
+
+var exec = require('child_process').exec;
+
+var result = function(command, cb){
+    var child = exec(command, function(err, stdout, stderr){
+        if(err != null){
+            return cb(new Error(err), null);
+        }else if(typeof(stderr) != "string"){
+            return cb(new Error(stderr), null);
+        }else{
+            return cb(null, stdout);
+        }
+    });
+}
+
+result("git checkout master", function(err, response) {
+    if(!err) {
+        console.log(response)
+    } else {
+        console.log("ERRO")
+        console.log(err)
+    }
+})
