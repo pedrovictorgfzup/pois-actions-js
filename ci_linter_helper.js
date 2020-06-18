34a5087dcfb4c6592592a3186bb144aa299969d3
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-06-18 16:15:26 -0300

    whay will happen?

--- ci_linter_helper.js
+++ ci_linter_helper.js
@@ -60,6 +60,7 @@ async function run_linter() {
 (async function main() {
     source_report, target_report = await run_linter()
 
+    console.log("JUST TO SEE")
     console.log("SOURCE: ", source_report)
     console.log("TARGET: ", target_report)
 })
