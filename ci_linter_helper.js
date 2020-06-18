7077bc5548a5ab36e3d600c026e2c0301ec0a15d
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-06-18 17:05:14 -0300

    ops

--- ci_linter_helper.js
+++ ci_linter_helper.js
@@ -63,16 +63,20 @@ async function run_linter(cb) {
 function do_linter_checks(source_report, target_report) {
     console.log("SOURCE: ", source_report)
     console.log("TARGET: ", target_report)
-    source_total_offenses = source_report
-                                        .map( file => file.errorCount)
-                                            .reduce((total, current) => {
-                                                return total + current
-                                            })
-    target_total_offenses = target_report
-                                        .map( file => file.errorCount)
-                                            .reduce((total, current) => {
-                                                return total + current
-                                            })
+    let source_total_offenses = source_report.then((files) => {
+        return files.map( file => file.errorCount)
+                    .reduce((total, current) => {
+                        return total + current
+                    })
+    })
+                                        
+    let target_total_offenses = target_report.then((files) => {
+        return files.map( file => file.errorCount)
+                    .reduce((total, current) => {
+                        return total + current
+                    })
+    })
+                                       
     console.log("SOURCE: ", source_total_offenses)
     console.log("TARGET: ", target_total_offenses)
 }
